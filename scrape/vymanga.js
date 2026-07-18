5af3fc1469a696bf9afcb4c4b0768c370a9b6277
HamzahSk <168325326+HamzahSk@users.noreply.github.com>
2026-07-18 22:42:05 +0700

    Last Sync: 2026-07-18 22:42 (Mobile)

--- scrape/vymanga.js
+++ scrape/vymanga.js
@@ -3,7 +3,6 @@ import path from 'path';
 
 // --- CONFIGURATION ---
 let baseUrl = "https://vymanga.net"; 
-const CORS = "https://cors-proxy1.rockyyrec.workers.dev/?url=";
 const headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
 };
